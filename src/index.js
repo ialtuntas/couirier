e3e66bc82a9db25d13e31af9f7d627781fa7da35
ialtuntas <ialtuntas1991@gmail.com>
2021-07-30 22:08:43 +0300

    route yapısı eklendi

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import Route from "./Router";
 import "./index.css";
-import App from "./App";
 import { I18nextProvider } from "react-i18next";
 import i18next from "i18next";
 import tr from "./translations/tr.json";
@@ -30,8 +31,12 @@ i18next.init({
   interpolation: { escapeValue: false },
 });
 ReactDOM.render(
-  <I18nextProvider i18n={i18next}>
-    <App />
-  </I18nextProvider>,
+  <React.StrictMode>
+    <BrowserRouter>
+      <I18nextProvider i18n={i18next}>
+        <Route component={Route} />
+      </I18nextProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
   document.getElementById("root")
 );
